524062f799276917346b29a862aeae8a81b3f9db
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-03-19 18:46:41 +0200

    generate ast file

--- src/parse.js
+++ src/parse.js
@@ -1,7 +1,13 @@
 const nearley = require('nearley');
 const grammar = require('./grammar.js');
+const fs = require('fs');
 
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 
-parser.feed(`hello("hello")`);
+parser.feed(`hello("hello" 2)`);
 console.log(parser.results);
+// Convert the results array to a JSON string
+const resultsJson = JSON.stringify(parser.results, null, 2);
+
+// Write the JSON string to a file
+fs.writeFileSync('./src/mo.ast', resultsJson);

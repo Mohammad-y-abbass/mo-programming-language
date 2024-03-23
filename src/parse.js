9afbd11a2b0b3055c256e2c70d78dd05c5c6df7c
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-03-23 18:43:23 +0200

    test input sample

--- src/parse.js
+++ src/parse.js
@@ -3,7 +3,7 @@ const grammar = require('./grammar.js');
 const fs = require('fs');
 
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
-const input = `when x > 1 =>
+const input = `while x > 1 =>
 call print("greater then")`;
 
 // Parse the input

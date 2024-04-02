18d6dac0a6e9af45a9852f582b2b4d31004d60b1
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-04-02 21:55:39 +0300

    temporarily remove transpile script from cli

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -5,23 +5,15 @@ const colors = require('colors')
 
 // Define filenames
 const grammarFile = 'src/grammar/grammar.ne';
-const sourceFile = 'src/example/example.txt';
 const generatedGrammar = 'src/grammar/grammar.js';
 const parserFile = 'src/parser/parse.js'
 const astFile = 'src/ast.json';
 const astToJsFile = 'src/transpiler/transpile.js';
-const outputFile = 'src/transpiler/transpiled.js';
 
 console.log('Generating grammar'.magenta);
 execSync(`nearleyc ${grammarFile} -o ${generatedGrammar}`);
 
 console.log('Generating parser'.magenta);
 execSync(`node ${parserFile}`);
-
-//console.log('Translating AST to JavaScript...'.magenta);
-//execSync(`node ${astToJsFile} ${astFile} > ${outputFile}`);
-
-//console.log('Executing JavaScript program...'.magenta);
-//execSync(`node ${outputFile}`);
 
 console.log('TRANSPILATION SUCCESSFUL |^_^|'.rainbow.bold);

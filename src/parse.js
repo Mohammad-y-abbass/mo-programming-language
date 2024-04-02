1f3ce27374fcee20da3e3b047b4511058930008f
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-04-02 21:13:43 +0300

    used colors library instead of chalk

--- src/parse.js
+++ src/parse.js
@@ -1,11 +1,11 @@
 const nearley = require('nearley');
 const grammar = require('./grammar.js');
 const fs = require('fs');
-
+const colors = require('colors');
 // Read the input file
 fs.readFile('src/example.txt', 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.error(err.red);
     return;
   }
 
@@ -15,15 +15,15 @@ fs.readFile('src/example.txt', 'utf8', (err, data) => {
   try {
     parser.feed(data);
     if (parser.results.length > 0) {
-      console.log('Parsing successful!');
+      console.log('Parsing successful!'.green.bold);
       const resultsJson = JSON.stringify(parser.results, null, 2);
 
       // Write the JSON string to a file
       fs.writeFileSync('./src/mo.ast', resultsJson);
     } else {
-      console.log('parser returned no results');
+      console.log('parser returned no results'.red.bold);
     }
   } catch (parseError) {
-    console.log('Error while parsing ' + parseError);
+    console.log(`Error while parsing ${parseError}`.bgRed);
   }
 });

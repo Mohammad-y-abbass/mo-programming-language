3cd73bc97846b25935df5ad6d9a0c880cf3a77c7
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-04-05 17:51:47 +0300

    fix cli log issue

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process');
-const colors = require('colors')
+const colors = require('colors');
 
 // Define filenames
 const grammarFile = 'src/grammar/grammar.ne';
 const generatedGrammar = 'src/grammar/grammar.js';
-const parserFile = 'src/parser/parse.js'
-const astFile = 'src/ast.json';
-const astToJsFile = 'src/transpiler/transpile.js';
+const parserFile = 'src/parser/parse.js';
 
+// Log messages with colors
 console.log('Generating grammar'.magenta);
-execSync(`nearleyc ${grammarFile} -o ${generatedGrammar}`);
+console.log(
+  execSync(`nearleyc ${grammarFile} -o ${generatedGrammar}`).toString()
+);
 
 console.log('Generating parser'.magenta);
-execSync(`node ${parserFile}`);
+console.log(execSync(`node ${parserFile}`).toString());
 
 console.log('TRANSPILATION SUCCESSFUL |^_^|'.rainbow.bold);

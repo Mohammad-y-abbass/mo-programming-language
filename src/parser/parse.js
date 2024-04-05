a2c54c57cbf1177086c8d190ad9fa4d956519df0
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-04-05 18:29:15 +0300

    add ascii art

--- src/parser/parse.js
+++ src/parser/parse.js
@@ -2,6 +2,7 @@ const nearley = require('nearley');
 const grammar = require('../grammar/grammar.js');
 const fs = require('fs');
 const colors = require('colors');
+
 // Read the input file
 fs.readFile('src/example/example.txt', 'utf8', (err, data) => {
   if (err) {
@@ -16,10 +17,40 @@ fs.readFile('src/example/example.txt', 'utf8', (err, data) => {
     parser.feed(data);
     if (parser.results.length > 0) {
       console.log('Parsing successful!'.green.bold);
+
       const resultsJson = JSON.stringify(parser.results, null, 2);
 
       // Write the JSON string to a file
       fs.writeFileSync('./src/ast.json', resultsJson);
+
+      // Define an ASCII art image
+      const coolArt = `                         
+                                                   
+                                                  
+                                                                 
+                                                                 
+MMMMMMMM               MMMMMMMM          OOOOOOOOO           !!! 
+M:::::::M             M:::::::M        OO:::::::::OO        !!:!!
+M::::::::M           M::::::::M      OO:::::::::::::OO      !:::!
+M:::::::::M         M:::::::::M     O:::::::OOO:::::::O     !:::!
+M::::::::::M       M::::::::::M     O::::::O   O::::::O     !:::!
+M:::::::::::M     M:::::::::::M     O:::::O     O:::::O     !:::!
+M:::::::M::::M   M::::M:::::::M     O:::::O     O:::::O     !:::!
+M::::::M M::::M M::::M M::::::M     O:::::O     O:::::O     !:::!
+M::::::M  M::::M::::M  M::::::M     O:::::O     O:::::O     !:::!
+M::::::M   M:::::::M   M::::::M     O:::::O     O:::::O     !:::!
+M::::::M    M:::::M    M::::::M     O:::::O     O:::::O     !!:!!
+M::::::M     MMMMM     M::::::M     O::::::O   O::::::O      !!! 
+M::::::M               M::::::M     O:::::::OOO:::::::O          
+M::::::M               M::::::M      OO:::::::::::::OO       !!! 
+M::::::M               M::::::M        OO:::::::::OO        !!:!!
+MMMMMMMM               MMMMMMMM          OOOOOOOOO           !!! 
+                                                          
+                                                                                                                                                                                  
+`;
+
+      // Print the ASCII art after parsing is done
+      console.log(coolArt.rainbow.bold);
     } else {
       console.log('parser returned no results'.red.bold);
     }
